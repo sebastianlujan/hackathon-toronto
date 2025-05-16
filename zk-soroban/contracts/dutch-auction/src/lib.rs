084f095b3dd1f67af4f6e7e93ed4e6c640970a78
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-05-16 10:17:49 -0300

    add it Auction States and DataKey

--- zk-soroban/contracts/dutch-auction/src/lib.rs
+++ zk-soroban/contracts/dutch-auction/src/lib.rs
@@ -10,16 +10,53 @@ use tfhe::prelude::*;
 pub struct Contract;
 
 
+// Data models
+
 #[derive(Clone)]
 #[contracttype]
-pub struct Auction {
-    pub highest_bid: U256,
-    pub highest_bidder: BytesN<32>,
-    pub end_time: u64,
+pub enum DataKey {
+    Admin,            // The auction administrator
+    TokenId,          // Payment token ID
+    ItemId,           // ID of the item being auctioned
+    StartTime,        // Auction start time
+    EndTime,          // Auction end time
+    StartPrice,       // Starting price
+    MinimumPrice,     // Minimum price
+    CurrentPrice,     // Current price
+    HighestBidder,    // Highest bidder
+    HighestBid,       // Highest bid
+    State,            // Auction state (CREATED, ACTIVE, ENDED)
+    PriceDecrement,   // Price decrement per time unit
 }
 
+
+#[derive(Clone, PartialEq)]
+#[contracttype]
+pub enum AuctionState {
+    Created,
+    Active,
+    Ended,
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub enum ErrorCode {
+    AlreadyInitialized,
+    NotAdmin,
+    InvalidState,
+    AuctionNotEnded,
+    AuctionEnded,
+    BidTooLow,
+    InsufficientFunds,
+}
+
+// ======== Contract Implementation ========
+#[contract]
+pub struct DutchAuction;
+
+
 #[contractimpl]
-impl Contract {
+impl DutchAuction {
 }
 
 mod test;
